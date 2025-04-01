0a66b43ef33f857a4eae8d12f0f1a6cfb1ec0a0f
Dylan Bv <89555477+DylanBvPro@users.noreply.github.com>
2025-04-01 15:53:34 -0400

    More concept ideas.

--- src/components/MadeForProject/autocomplete.js
+++ src/components/MadeForProject/autocomplete.js
@@ -1,4 +1,5 @@
 // autocomplete.js
+const basePath = import.meta.env.PROD ? '/Genesis/' : '/';
 export const items = ['option1', 'option2', 'option3', 'option4', 'option5'];
 
 export function showSuggestions(query) {
@@ -13,7 +14,7 @@ export function showSuggestions(query) {
         item.className = 'autocomplete-item';
         item.textContent = suggestion;
         item.onclick = () => {
-            const searchInput = document.getElementById('searchInput');
+            const searchInput = document.getElementById('searchbutton');
             if (searchInput instanceof HTMLInputElement) {
                 searchInput.value = suggestion;
             }
@@ -21,4 +22,19 @@ export function showSuggestions(query) {
         };
         autocompleteList.appendChild(item);
     });
+}
+
+export function executeSearch() {
+    console.log('Ran Search!');
+    const searchInput = document.getElementById('searchbutton');
+    window.location.href = `${basePath}searchresult`;
+    if (searchInput instanceof HTMLInputElement) {
+        const query = searchInput.value;
+        // Add your logic here to handle different options based on the search query
+        if (items.includes(query)) {
+            window.location.href = `${basePath}searchresult`;
+        } else {
+            alert('No matching option found');
+        }
+    }
 }
